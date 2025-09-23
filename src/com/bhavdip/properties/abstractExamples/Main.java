f938dd52f55af0f53dc40d33527bff1e8fdea27e
Bhavdip Gediya <bhavdipgediya098@gmail.com>
2025-09-23 21:10:48 +0530

    encapsulation concept

--- src/com/bhavdip/properties/abstractExamples/Main.java
+++ src/com/bhavdip/properties/abstractExamples/Main.java
@@ -1,5 +1,7 @@
 package com.bhavdip.properties.abstractExamples;
 
+//Abstraction means hiding implementation details and showing only the essential features to the user.
+//In simple terms: You focus on what an object does, not how it does it.
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/com/bhavdip/properties/encapsulation/Main.java
@@ -0,0 +1,11 @@
+package com.bhavdip.properties.encapsulation;
+
+//Encapsulation is the concept of wrapping data (fields/variables) and behavior (methods) together into a single unit (class)
+//and restricting direct access to some of the object’s components
+
+//--> Achieved using access modifiers (private, public, protected).
+//--> Protects data from unauthorized access or modification.
+//--> Provides data hiding + controlled access.
+
+public class Main {
+}
