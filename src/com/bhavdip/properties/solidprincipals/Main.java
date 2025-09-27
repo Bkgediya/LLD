699801cc67222d195b5d332b36decd1a70afcde7
Bhavdip Gediya <bhavdipgediya099@gmail.com>
2025-09-27 21:11:14 +0530

    solid principal full forms

--- src/com/bhavdip/properties/solidprincipals/Main.java
+++ src/com/bhavdip/properties/solidprincipals/Main.java
@@ -1,5 +1,12 @@
 package com.bhavdip.properties.solidprincipals;
 
+
+// s : single responsibility principal
+// o : open/close principal
+// L : Liskov substitution principal (LSP)
+// i : Interface segregation principal (ISP)
+// d : Dependency inversion principal
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
