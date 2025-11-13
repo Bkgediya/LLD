9e641effde1ef52a0be589f718016a1e9c5f147e
Bhavdip Gediya <bhavdipgediya099@gmail.com>
2025-11-13 08:13:08 -0800

    singleton is created

---
+++ Singleton/Singleton.java
@@ -0,0 +1,25 @@
+public class Singleton {
+
+    private static Singleton instance = null;
+
+    private Singleton() {
+        System.out.println("Singleton instance created");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+}
